9cbbcd670a5689342527a398b2798a9d905081ab
KELLY LUU <kellyluuu@gmail.com>
2022-07-16 15:03:33 -0500

    upload to amazon s3

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
-from .models import Cat, Toy
+from .models import Cat, Toy, Photo
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+import uuid
+import boto3
 
 
-# Import the FeedingForm
+
 from .forms import FeedingForm
+
+# Add these "constants" below the imports
+S3_BASE_URL = 'https://s3.us-east-1.amazonaws.com/'
+BUCKET = 'mycatcollector'
 
 
 
@@ -46,6 +52,27 @@ def assoc_toy(request, cat_id, toy_id):
   # Note that you can pass a toy's id instead of the whole object
   Cat.objects.get(id=cat_id).toys.add(toy_id)
   return redirect ('detail', cat_id=cat_id)
+
+def add_photo(request, cat_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            photo = Photo(url=url, cat_id=cat_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail', cat_id=cat_id)
+  
+   
 
 class CatCreate(CreateView):
   model = Cat
